d2815d7e5944fea746f334597cb14e4789f0bb15
Roshan Kumar <rsahu7989@gmail.com>
2025-08-18 00:51:54 +0530

    add database management scripts and remove unused config file

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-const { PORT, HOST } = process.env;
-export const Config = {
-	PORT,
-	HOST,
-	URL: `http://${HOST}:${PORT}`,
-	NODE_ENV: process.env.NODE_ENV,
-};
